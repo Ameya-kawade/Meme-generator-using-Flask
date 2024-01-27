6a5a4c17925dedff717f28027258182534ccc997
Ameya Kawade <ameyakawade0@gmail.com>
2024-01-27 08:48:33 +0530

    added icom and keywords to improve seo performance

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 from flask import Flask, render_template,jsonify
 import requests
+from random import randint
 
 app = Flask(__name__)
 
 def get_meme():
+    memes = ['memes', 'dankmemes', 'me_irl','wholesomememes']
     title = ""
     image = ""
-    url = "https://meme-api.com/gimme"
+    url = f"https://meme-api.com/gimme/{memes[randint(0,len(memes) - 1)]}"
     response = requests.get(url)
     if response.status_code == 200:
         data  = response.json()
@@ -32,5 +34,5 @@ def send_meme():
     return jsonify(data)
 
 
-# if __name__ == "__main__":
-#     app.run(debug = True)
+if __name__ == "__main__":
+    app.run(debug = True)
